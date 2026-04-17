fd18c24ab10ee581a7ded8f018f442e6069ceea2
JuanDiegoGutierrezDuenas <est.juand.gutierrezd@unimilitar.edu.co>
2026-04-17 09:41:39 -0500

    Se modifico el codigo para que funcione con la libreria proporcionada por el docente

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,27 @@
 import java.util.*;
-import com.murcia.utils.*;
+import java.io.*;
+import com.murcia.utils.*; 
 
-class Main {
-    public static void main(String[] args) throws Exception{
-        File file=new File("randomtext.txt");
-        Scanner sc=new Scanner(file);
-        LinkedList<StringBuilder> l=new LinkedList<StringBuilder>();
+public class Main {
+    public static void main(String[] args) throws Exception {
+        File file = new File("randomtext.txt");
+        Scanner sc = new Scanner(file);
+
+        ColaEnlazada<StringBuilder> l = new ColaEnlazada<>();
+
         while(sc.hasNextLine()) {
-            String line=sc.nextLine();
-            String[] words=line.split("\\s");
-            for(String i:words) 
-                l.add(new StringBuilder(i));
-            l.add(new StringBuilder("\n"));
+            String line = sc.nextLine();
+            String[] words = line.split("\\s");
+            for(String i : words) {
+                l.encolar(new StringBuilder(i));
+            }
+            l.encolar(new StringBuilder("\n"));
         }
-        Editor edit=new Editor(l);
-        sc=new Scanner(System.in);
-        while(true)
-        {
+
+        Editor edit = new Editor(l);
+        sc = new Scanner(System.in);
+
+        while(true) {
             System.out.println("S - Save and Exit");
             System.out.println("F - Move Forward");
             System.out.println("B - Move Backward");
@@ -24,9 +29,11 @@ class Main {
             System.out.println("e - Move to End of Line");
             System.out.println("i - Insert");
             System.out.println("d - Delete");
+
             edit.print();
             edit.printCursor();
-            String input=sc.nextLine();
+
+            String input = sc.nextLine();
             if(input.equals("S")) break;
             else if(input.equals("F")) edit.forward();
             else if(input.equals("B")) edit.backward();
@@ -34,93 +41,102 @@ class Main {
             else if(input.equals("e")) edit.ending();
             else if(input.equals("i")) edit.insert(sc.nextLine());
             else if(input.equals("d")) edit.delete();
-            System.out.print("\033[H\033[2J");  
-            System.out.flush();
+
+            Consola.clrscr();
+
             file.delete();
             file.createNewFile();
         }
         edit.writer.close();
     }
 }
+
 class Editor {
-    LinkedList<StringBuilder> l;
-    int node,position;
+    ColaEnlazada<StringBuilder> l;
+    int node, position;
     FileWriter writer;
-    public Editor(LinkedList<StringBuilder> l) {
-        this.l=l;
-        node=position=0;
+
+    public Editor(ColaEnlazada<StringBuilder> l) {
+        this.l = l;
+        node = position = 0;
     }
-    public void forward(){
-        StringBuilder s=l.get(node);
-        if(position==s.length()) {
-            if(node==l.size()-1) node=0;
+
+    public void forward() {
+        StringBuilder s = l.get(node);
+        if(position == s.length()) {
+            if(node == l.size() - 1) node = 0;
             else node++;
-            position=0;
-        }
-        else position++;
-        s=l.get(node);
+            position = 0;
+        } else position++;
+        s = l.get(node);
         if(s.toString().equals("\n")) forward();
     }
-    public void backward(){
-        StringBuilder s=l.get(node);
-        if(position==0) {
-            if(node==0) node=l.size()-1;
+
+    public void backward() {
+        StringBuilder s = l.get(node);
+        if(position == 0) {
+            if(node == 0) node = l.size() - 1;
             else node--;
-            s=l.get(node);
-            position=s.length();
-        }
-        else position--;
-        s=l.get(node);
+            s = l.get(node);
+            position = s.length();
+        } else position--;
+        s = l.get(node);
         if(s.toString().equals("\n")) backward();
     }
+
     public void insert(String input) {
-        StringBuilder s=l.get(node);
-        if(s.length()==0) s.append(input);
-        else s.insert(position,input);
-        position+=input.length();
+        StringBuilder s = l.get(node);
+        if(s.length() == 0) s.append(input);
+        else s.insert(position, input);
+        position += input.length();
     }
+
     public void delete() {
-        StringBuilder s=l.get(node);
-        if(position==0 && node>0){
-            StringBuilder prev=l.get(node-1);
-            position=prev.length();
+        StringBuilder s = l.get(node);
+        if(position == 0 && node > 0) {
+            StringBuilder prev = l.get(node - 1);
+            position = prev.length();
             prev.append(s);
             l.remove(node);
-        }
-        else if(position>0){
-            s.delete(position-1,position);
+        } else if(position > 0) {
+            s.delete(position - 1, position);
             position--;
         }
     }
+
     public void beginning() {
-       while(node>0 && !l.get(node).toString().equals("\n")) node--;
-       if(node!=0) node=node+1;
-       position=0;
+        while(node > 0 && !l.get(node).toString().equals("\n")) node--;
+        if(node != 0) node = node + 1;
+        position = 0;
     }
+
     public void ending() {
-       while(!l.get(node).toString().equals("\n")) node++;
-       node=node-1;
-       position=l.get(node).length();
+        while(!l.get(node).toString().equals("\n")) node++;
+        node = node - 1;
+        position = l.get(node).length();
     }
-    public void print() throws Exception{
-        writer= new FileWriter("randomtext.txt");
-        for(StringBuilder i:l) {
-            writer.write(i.toString());
-            System.out.print(i);
-            if(!i.toString().equals("\n")) {
+
+    public void print() throws Exception {
+        writer = new FileWriter("randomtext.txt");
+        for(int i = 0; i < l.size(); i++) {
+            StringBuilder sb = l.get(i);
+            writer.write(sb.toString());
+            System.out.print(sb);
+            if(!sb.toString().equals("\n")) {
                 writer.write(" ");
                 System.out.print(" ");
             }
         }
     }
+
     public void printCursor() {
         System.out.print("Cursor At : ");
-        StringBuilder s=l.get(node);
-        for(int i=0; i<s.length(); i++) {
-            if(i==position) System.out.print("|");
+        StringBuilder s = l.get(node);
+        for(int i = 0; i < s.length(); i++) {
+            if(i == position) System.out.print("|");
             System.out.print(s.charAt(i));
         }
-        if((position==0 && s.length()==0) || (position==s.length())) System.out.print("|");
+        if((position == 0 && s.length() == 0) || (position == s.length())) System.out.print("|");
         System.out.println();
     }
 }
